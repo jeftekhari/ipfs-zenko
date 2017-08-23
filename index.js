3b9bdfee9f9a71309af115a3ea47f80e88662e4a
Hanu Prateek Kunduru <hanu.prateek@gmail.com>
2017-08-22 20:44:51 -0700

    update base code with ipfs functions

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-'use strict'; // eslint-disable-line strict
+'use strict';
 
 const ipfsAPI = require('ipfs-api');
 const arsenal = require('arsenal');
 const werelogs = require('werelogs');
-// const Memcached = require('memcached');
+const errors = require('arsenal/errors');
 
 const SUBLEVEL_SEP = '::';
-// const MEMCACHED_LIFETIME = 100000;
 
 const logOptions = {
     "logLevel": "debug",
@@ -17,106 +16,65 @@ const logger = new werelogs.Logger('Zenko-IPFS');
 
 // Metadata
 
-const MetadataFileServer =
-          require('arsenal').storage.metadata.MetadataFileServer;
+const MetadataFileServer = require('arsenal').storage.metadata.MetadataFileServer;
 
-const mdServer = new MetadataFileServer(
-    { bindAddress: '0.0.0.0',
-      port: 9990,
-      path: '/tmp',
-      restEnabled: false,
-      restPort: 9999,
-      recordLog: { enabled: false, recordLogName: 's3-recordlog' },
-      versioning: { replicationGroupId: 'RG001' },
-      log: logOptions });
+const mdServer = new MetadataFileServer({
+    bindAddress: '0.0.0.0',
+    port: 9990,
+    path: '/tmp',
+    restEnabled: false,
+    restPort: 9999,
+    recordLog: { enabled: false, recordLogName: 's3-recordlog' },
+    versioning: { replicationGroupId: 'RG001' },
+    log: logOptions
+});
 
-// var memcached = new Memcached('localhost:11211', {retries:10,retry:10000,remove:true,failOverServers:['192.168.0.103:11211']});
-var ipfs = ipfsAPI('localhost', '5001', {protocol: 'http'})
-// var ipfs = ipfsAPI('/ip4/127.0.0.1/tcp/5001')
+var ipfs = ipfsAPI('/ip4/127.0.0.1/tcp/5001');
 
 class IPFSService extends arsenal.network.rpc.BaseService {
     constructor(params) {
-    super(params);
-    this.addRequestInfoConsumer((dbService, reqParams) => {
+        super(params);
+        this.addRequestInfoConsumer((dbService, reqParams) => {
             const env = {};
             env.subLevel = reqParams.subLevel;
-        return env;
-    });
+            return env;
+        });
     }
 }
 
-mdServer.initMetadataService = function ()
-{
+mdServer.initMetadataService = function() {
     const dbService = new IPFSService({
-    namespace: '/MDFile/metadata',
-    logger: logger
+        namespace: '/MDFile/metadata',
+        logger: logger
     });
     this.services.push(dbService);
 
     dbService.registerAsyncAPI({
         put: (env, key, value, options, cb) => {
-        const dbName = env.subLevel.join(SUBLEVEL_SEP);
-        console.log('put',env,dbName,key,value,options);
-/*      memcached.get(dbName, (err, data) => {
-        if (err) {
-            console.log(err);
-            let db = {};
-            db[key] = value;
-            memcached.add(dbName, JSON.stringify(db), MEMCACHED_LIFETIME, 
-                  (err) => {
-                      if (err) {
-                      console.log(err);
-                      cb(err);
-                      } else {
-                      cb(null);
-                      }
-                  });
-        } else {
-            console.log(data);
-            let db = JSON.parse(data);
-            db[key] = value;
-            memcached.replace(dbName, JSON.stringify(db), MEMCACHED_LIFETIME, 
-                      (err) => {
-                      if (err) {
-                          console.log(err);
-                          cb(err);
-                      } else {
-                          cb(null);
-                      }
-                      });
-        }
-        });*/
+            const dbName = env.subLevel.join(SUBLEVEL_SEP);
+            console.log('put',env,dbName,key,value,options);
         },
         del: (env, key, options, cb) => {
-        console.log('del',env,key,options);
+            console.log('del',env,key,options);
         },
         get: (env, key, options, cb) => {
-        console.log('get',key,options);
-/*      memcached.get(dbName, (err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(data);
-            let db = JSON.parse(data);
-            cb(null, db[key])
-        }
-        });
-*/
+            console.log('get',key,options);
         },
-    getDiskUsage: (env, cb) => {
-        console.log('getDiskUsage',env);
-    },
+        getDiskUsage: (env, cb) => {
+            console.log('getDiskUsage',env);
+        },
     });
+
     dbService.registerSyncAPI({
         createReadStream:
         (env, options) => {
-        console.log('createReadStream');
-    },
+            console.log('createReadStream');
+        },
         getUUID: () => this.readUUID(),
     });
     
     console.log('Hooks installed');
-}
+};
 
 mdServer.startServer();
 
@@ -124,48 +82,74 @@ mdServer.startServer();
 
 class IPFSFileStore extends arsenal.storage.data.file.DataFileStore {
     constructor(dataConfig, logApi) {
-    super(dataConfig, logApi);
-    console.log('filestore constructor');
+        super(dataConfig, logApi);
+        console.log('filestore constructor');
     }
 
     setup(callback) {
-    console.log('data setup');
-    callback(null);
+        console.log('data setup');
+        callback(null);
     }
 
     put(dataStream, size, log, callback) {
-    console.log('data put');
+        console.log('data put');
+        const files = [
+            {
+                path: '/tmp/myfile',
+                content: dataStream
+            }
+        ];
+
+        ipfs.files.add(files, function (err, files) {
+            if (err) {
+                log.error('error putting files',
+                          { method: 'put', key, error: err });
+                return callback(errors.InternalError.customizeDescription(
+                    `filesystem error: open() returned ${err.code}`));
+            }
+        });
     }
 
     stat(key, log, callback) {
-    console.log('data stat');
+        console.log('data stat');
     }
 
     get(key, byteRange, log, callback) {
-    console.log('data get');
+        console.log('data get');
+
+        const filePath = this.getFilePath(key);
+        const cbOnce = jsutil.once(callback);
+        ipfs.files.get(key, function (err, stream) {
+            stream.on('data', (file) => {
+            // write the file's path and contents to standard out
+                console.log(file.path)
+                file.content.pipe(process.stdout)
+            })
+        })
     }
 
     delete(key, log, callback) {
-    console.log('data delete');
+        console.log('data delete');
     }
 
     getDiskUsage(callback) {
-    console.log('data getDiskUsage');
+        console.log('data getDiskUsage');
     }
 }
 
-const dataServer = new arsenal.network.rest.RESTServer(
-    { bindAddress: '0.0.0.0',
-      port: 9991,
-      dataStore: new IPFSFileStore(
-          { dataPath: '/tmp',
-            log: logOptions }),
-      log: logOptions });
+const dataServer = new arsenal.network.rest.RESTServer({
+    bindAddress: '0.0.0.0',
+    port: 9991,
+    dataStore: new IPFSFileStore({ 
+        dataPath: '/tmp',
+        log: logOptions
+    }),
+    log: logOptions
+});
 
 dataServer.setup(err => {
     if (err) {
-        logger.error('Error initializing REST data server',
-                     { error: err });
+        logger.error('Error initializing REST data server', { error: err });
         return;
     }
 
